eb9500ec0312608c65a955108043da936bd4213d
ShihangWu <2478677199@qq.com>
2026-05-17 18:31:11 +0800

    Fix: use hash router mode for GitHub Pages compatibility

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
@@ -7,7 +7,7 @@ import Home from './views/Home.vue'
 import Result from './views/Result.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
